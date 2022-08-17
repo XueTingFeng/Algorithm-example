54221eaac6c50ac7c51536e3aff9ae4bf626027c
coderxue666 <coderxue@qq.com>
2022-08-17 21:21:45 +0800

    贪心算法

---
+++ src/com/atguigu/greedy/GreedyAlgorithm.java
@@ -0,0 +1,86 @@
+﻿package com.atguigu.greedy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * @author shkstart
+ * @create 2022-08-17 20:55
+ */
+public class GreedyAlgorithm {
+    public static void main(String[] args) {
+        HashMap<String, HashSet<String>> broadcasts = new HashMap<>();
+        //将各个电台放入到broadcasts
+        HashSet<String> hashSet1 = new HashSet<>();
+        hashSet1.add("北京");
+        hashSet1.add("上海");
+        hashSet1.add("天津");
+
+        HashSet<String> hashSet2 = new HashSet();
+        hashSet2.add("广州");
+        hashSet2.add("北京");
+        hashSet2.add("深圳");
+
+        HashSet<String> hashSet3 = new HashSet();
+        hashSet3.add("成都");
+        hashSet3.add("上海");
+        hashSet3.add("杭州");
+
+        HashSet<String> hashSet4 = new HashSet();
+        hashSet4.add("上海");
+        hashSet4.add("天津");
+
+        HashSet<String> hashSet5 = new HashSet();
+        hashSet5.add("杭州");
+        hashSet5.add("大连");
+
+        broadcasts.put("K1",hashSet1);
+        broadcasts.put("K2",hashSet2);
+        broadcasts.put("K3",hashSet3);
+        broadcasts.put("K4",hashSet4);
+        broadcasts.put("K5",hashSet5);
+
+        //存放所有地区
+        HashSet<String> allAreas = new HashSet<>();
+        allAreas.add("北京");
+        allAreas.add("上海");
+        allAreas.add("天津");
+        allAreas.add("广州");
+        allAreas.add("深圳");
+        allAreas.add("成都");
+        allAreas.add("杭州");
+        allAreas.add("大连");
+
+        //创建ArrayList,存放选择的电台集合
+        ArrayList<String> selects = new ArrayList<>();
+
+        //定义临时的集合,在遍历的过程中，存放遍历过程中覆盖的地区和当前未覆盖地区的交集
+        HashSet<String> tempSet = new HashSet<>();
+
+        //定义maxkey,最大未覆盖的key
+        String maxKey = null;
+        while (allAreas.size() != 0){
+            maxKey = null;
+            //遍历broadcasts,取出对应的key
+            for (String key :
+                    broadcasts.keySet()) {
+                tempSet.clear();
+                HashSet<String> areas = broadcasts.get(key);
+                tempSet.addAll(areas);
+                //求出交集,交集会赋给tempSet
+                tempSet.retainAll(allAreas);
+                if(tempSet.size() > 0 &&
+                        (maxKey == null || tempSet.size() > broadcasts.get(maxKey).size())){
+                    maxKey = key;
+                }
+            }
+            //将maxKey加入selects
+            if(maxKey != null){
+                selects.add(maxKey);
+                //将maxkey指向的广播覆盖的地区,从allAreas去掉
+                allAreas.removeAll(broadcasts.get(maxKey));
+            }
+        }
+    }
+}
