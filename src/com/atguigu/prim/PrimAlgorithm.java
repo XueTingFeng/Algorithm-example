10fc9fbb9d60792fcda2333c288530af9436b91e
xuetingfeng <coderxue@qq.com>
2022-08-22 16:02:03 +0800

    prim

--- src/com/atguigu/prim/PrimAlgorithm.java
+++ src/com/atguigu/prim/PrimAlgorithm.java
@@ -27,6 +27,8 @@ public class PrimAlgorithm {
         MinTree minTree = new MinTree();
         minTree.createGraph(mGraph,verxs,data,weight);
         minTree.showGraph(mGraph);
+
+        minTree.prim(mGraph,0);
     }
 }
 
@@ -55,6 +57,43 @@ class MinTree{
     public void showGraph(MGraph graph){
         for (int[] link : graph.weight) {
             System.out.println(Arrays.toString(link));
+        }
+    }
+
+    /**
+     * 编写prim算法,得到最小生成树
+     * @param graph 图
+     * @param v 表示从图的第几个顶点开始生成'A'->0 'B'->1
+     */
+    public void prim(MGraph graph,int v){
+        //标记节点是否被访问过
+        int[] visited = new int[graph.verxs];
+        //visited[]默认元素的值都是0，表示没有访问过
+
+        //把当前节点标记为已访问
+        visited[v] = 1;
+        //记录两个顶点的下标
+        int h1 = -1;
+        int h2 = -1;
+        int minWeight = 10000;//将minweight初始成一个较大数
+        for (int k = 1; k < graph.verxs; k++) {//graph.verxs-1 条边
+
+            //确定每一次生成的子图,和哪个节点的距离最近
+            for (int i = 0; i < graph.verxs; i++) {//i节点表示被访问过的节点
+                for (int j = 0; j < graph.verxs; j++) {//j节点表示还未访问过的节点
+                    if(visited[i] == 1 && visited[j] == 0 && graph.weight[i][j] < minWeight){
+                        minWeight = graph.weight[i][j];
+                        h1 = i;
+                        h2 = j;
+                    }
+                }
+            }
+            //找到一条边最小
+            System.out.println("边<" + graph.data[h1] + "," + graph.data[h2] + "> 权值:" + minWeight);
+            //把当前节点标记为已经访问
+            visited[h2] = 1;
+            //重置minWeight
+            minWeight = 10000;
         }
     }
 }
