eedd66e325522192e8c1d8def5f943546c9b1381
xuetingfeng <coderxue@qq.com>
2022-09-02 16:15:19 +0800

    迪杰斯特拉

--- out/production/Data/com/leetcode/linkedlist/141环形链表/141.环形链表.js
+++ out/production/Data/com/leetcode/linkedlist/141环形链表/141.环形链表.js
@@ -11,18 +11,37 @@
  * @return {boolean}
  */
 const hasCycle = function(head) {
-    let set = new Set()
-    while(head != null){
-        if(set.has(head)){
-            return true
-        } else {
-            set.add(head)
-        }
-        head = head.next
-    }
-    return false
 
-//标记法
+    //快慢指针
+    if (head == null || head.next == null) {
+        return false
+    }
+
+    let slow = head
+    let fast = head.next.next;
+
+    while (slow != fast){
+        if (fast == null || fast.next == null) {
+            return false
+        }
+        slow = slow.next
+        fast = fast.next.next
+    }
+    return true;
+
+    //哈希表
+    // let set = new Set()
+    // while(head != null){
+    //     if(set.has(head)){
+    //         return true
+    //     } else {
+    //         set.add(head)
+    //     }
+    //     head = head.next
+    // }
+    // return false
+
+    //标记法
     // while(head){
     //     if(head.tag){
     //         return true
