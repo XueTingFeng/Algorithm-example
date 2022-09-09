d278607b28e2c904cb43819c187f0d05807b730c
xuetingfeng <coderxue@qq.com>
2022-09-09 16:00:53 +0800

    136只出现一次的数字

--- src/com/leetcode/136.只出现一次的数字.js
+++ src/com/leetcode/136.只出现一次的数字.js
@@ -3,13 +3,19 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    const set = new Set()
-    for(let i=0;i<nums.length;i++){
-        if(set.has(nums[i])){
-            set.delete(nums[i])
-        } else {
-            set.add(nums[i])
-        }
-    }
-    return [...set].pop()
+    // const set = new Set()
+    // for(let i=0;i<nums.length;i++){
+    //     if(set.has(nums[i])){
+    //         set.delete(nums[i])
+    //     } else {
+    //         set.add(nums[i])
+    //     }
+    // }
+    // return [...set].pop()
+
+    let single = 0
+    nums.forEach((el) =>{
+        single ^= el
+    })
+    return single
 };
