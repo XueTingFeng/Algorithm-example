3313a61643d1b6331bc89fb72db6e0cc888dc9a5
xuetingfeng <coderxue@qq.com>
2022-11-18 10:19:21 +0800

    '前中后序迭代遍历'

--- src/com/leetcode/tree/二叉树前中后序遍历.js
+++ src/com/leetcode/tree/二叉树前中后序遍历.js
@@ -1,3 +1,9 @@
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
 let preRes = []
 const preOrder = (root) =>{
     if(root === null){
@@ -26,4 +32,54 @@ const postOrder = (root) => {
     postOrder(root.left)
     postOrder(root.right)
     postRes.push(root)
+}
+
+//前序遍历迭代
+const preorderTraversal = (root, res = []) =>{
+    if(!root){
+        return res
+    }
+    const stack = [root]
+    let cur = null
+    while (stack.length){
+        cur = stack.pop()
+        res.push(cur.val)
+        cur.right && stack.push(cur.right)
+        cur.left && stack.push(cur.left)
+    }
+    return res
+}
+
+//中序遍历迭代
+const inorderTraversal = (root, res = []) => {
+    const stack = []
+    let cur = root
+    while (stack.length || cur){
+        if(cur){
+            stack.push(cur)
+            cur = cur.left
+        } else {
+            cur = stack.pop()
+            res.push(cur.val)
+            cur = cur.right
+        }
+    }
+    return res
+}
+
+//后序遍历迭代
+const postorderTraversal = (root) => {
+    if(!root){
+        return res
+    }
+    const stack = [root]
+    let cur = null
+    while (stack.length){
+        cur = stack.pop()
+        res.push(cur.val)
+        cur.left && stack.push(cur.left)
+        cur.right && stack.push(cur.right)
+    }
+    res.reverse()
+    return res
 }
