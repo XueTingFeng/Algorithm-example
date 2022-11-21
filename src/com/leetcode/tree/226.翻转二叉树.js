3ea21debe749285b195f0dd20400e01a5b9293f3
xuetingfeng <coderxue@qq.com>
2022-11-21 09:48:07 +0800

    '翻转二叉树'

--- src/com/leetcode/tree/226.翻转二叉树.js
+++ src/com/leetcode/tree/226.翻转二叉树.js
@@ -24,3 +24,53 @@ var invertTree = function(root) {
     return root
 
 };
+
+//交换节点
+const invertNode = (root,left,right) =>{
+    let temp = left
+    left = right
+    right = temp
+    root.left = left
+    root.right = right
+}
+//前序遍历迭代
+const invertTree2 = function(root) {
+    let stack = []
+    if(root === null){
+        return root
+    }
+    stack.push(root)
+    while (stack.length){
+        let node = stack.pop()
+        if(node !== null){
+            node.right && stack.push(node.right)
+            node.left && stack.push(node.left)
+            stack.push(node)
+            stack.push(null)
+        } else {
+            node = stack.pop()
+            invertNode(node,node.left,node.right)
+        }
+    }
+
+    return root
+};
+
+//层序遍历
+const invertTree3 = (root) => {
+    let queue = []
+    if(root === null){
+        return root
+    }
+    queue.push(root)
+    while (queue.length){
+        let length = queue.length
+        while (length--){
+            let node = queue.shift()
+            invertNode(node,node.left,node.right)
+            node.left && queue.push(node.left)
+            node.right && queue.push(node.right)
+        }
+    }
+    return root
+}
