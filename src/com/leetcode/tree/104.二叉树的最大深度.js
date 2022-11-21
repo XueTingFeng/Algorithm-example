942484f9e66740b4facb1084c785da0e9df75a5f
xuetingfeng <coderxue@qq.com>
2022-11-21 11:03:33 +0800

    '二叉树的最大深度'

--- src/com/leetcode/tree/104.二叉树的最大深度.js
+++ src/com/leetcode/tree/104.二叉树的最大深度.js
@@ -14,3 +14,57 @@ const maxdepth = (root) => {
 
     return getDepth(root)
 }
+
+const maxdepth2 = (root) => {
+    let queue = []
+    let count = 0
+    queue.push(root)
+    if(root === null){
+        return 0
+    }
+    while (queue.length !== 0){
+        //记录当前层级节点数
+        let length = queue.length
+        //存放每一层的节点
+        count ++
+        for(let i=0;i<length;i++) {
+            let node = queue.shift()
+            for (let item of node.children) {
+                item && queue.push(item);
+            }
+        }
+    }
+    return count
+}
+
+//559 n叉树的最大深度
+const maxdepth2 = (root) => {
+    if(!root) return 0
+    let depth = 0
+    for(let node of root.children){
+        depth = Math.max(depth,maxdepth2(node))
+    }
+    return depth + 1
+}
+
+const maxdepth3 = (root) => {
+    let queue = []
+    let count = 0
+    queue.push(root)
+    if(root === null){
+        return 0
+    }
+    while (queue.length !== 0){
+        //记录当前层级节点数
+        let length = queue.length
+        //存放每一层的节点
+        count ++
+        for(let i=0;i<length;i++) {
+            let node = queue.shift()
+            for (let item of node.children) {
+                item && queue.push(item);
+            }
+        }
+    }
+    return count
+}
