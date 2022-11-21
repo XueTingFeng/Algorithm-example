6d9b7bb7dce3789f97a3a16bdf99ac3f9d450c69
xuetingfeng <coderxue@qq.com>
2022-11-21 10:13:55 +0800

    '对称二叉树'

--- src/com/leetcode/tree/101.对称二叉树.js
+++ src/com/leetcode/tree/101.对称二叉树.js
@@ -26,3 +26,27 @@ const isMirror = (p,q) => {
 
     return p.val === q.val && isMirror(p.left,q.right) && isMirror(p.right,q.left)
 }
+
+//递归判断是否为对称二叉树
+const isSymmetric2 = (root) => {
+    const compareNode = (left,right) => {
+        if(left === null && right !== null || left !== null && right === null){
+            return false
+        } else if (left === null && right === null){
+            return true
+        } else if (left.val !== right.val){
+            return false
+        }
+
+        let outSide = compareNode(left.left,right.right)
+        let inSide = compareNode(left.right,right.left)
+
+        return outSide && inSide
+    }
+
+    if(root === null){
+        return true
+    }
+
+    return compareNode(root.left,root.right)
+}
