61e22d3b0c5a034852b1308c260eb9fa85a16bc3
xuetingfeng <coderxue@qq.com>
2022-11-25 11:08:09 +0800

    '113.路径总和ii'

--- src/com/leetcode/tree/112.路径总和.js
+++ src/com/leetcode/tree/112.路径总和.js
@@ -40,3 +40,66 @@ const hasPathSu2 = function(root, targetSum){
     if(!root.left && !root.right && count === 0) return true
     return hasPathSu2(root.left,targetSum - root.val) || hasPathSu2(root.right,targetSum - root.val)
 }
+
+//113.路径总和ii
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+let pathSum = function (root,targetSum){
+    const res = []
+    const travelsal = (node,cnt,path) => {
+        if(!node.left && !node.right && cnt === 0){
+            res.push([...path])
+            return
+        }
+        if(!node.left && !node.right) return
+
+        if(node.left){
+            path.push(node.left.val)
+            travelsal(node.left,cnt - node.left.val, path)
+            path.pop()
+        }
+
+        if(node.right){
+            path.push(node.right.val)
+            travelsal(node.right,cnt - node.right.val, path)
+            path.pop()
+        }
+    }
+
+    if(!root) return res
+    travelsal(root, targetSum - root.val, [root.val])
+    return res
+}
+
+//精简版本
+const pathsum2 = function(root, targetSum){
+    let res = [],curPath = []
+    const travelTree = function(node,count){
+        curPath.push(node.val)
+        count -= node.val
+        if(!node.left && !node.right && count === 0){
+            res.push([...curPath])
+        }
+        node.left && travelTree(node.left,count)
+        node.right && travelTree(node.right,count)
+        let cur = curPath.pop()
+        count -= cur
+    }
+
+    if(root===null){
+        return res;
+    }
+    travelTree(root,targetSum);
+    return res;
+}
