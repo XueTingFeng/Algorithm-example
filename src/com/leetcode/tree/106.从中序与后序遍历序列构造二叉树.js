02f557c2226d252df7a258ac9bd089299efbbfbd
xuetingfeng <coderxue@qq.com>
2022-12-09 10:02:12 +0800

    '106.从中序与后序遍历序列构造二叉树.js'

--- src/com/leetcode/tree/106.从中序与后序遍历序列构造二叉树.js
+++ src/com/leetcode/tree/106.从中序与后序遍历序列构造二叉树.js
@@ -7,3 +7,34 @@ var buildTree = function(inorder, postorder) {
     root.right = buildTree(inorder.slice(rootIndex + 1), postorder.slice(rootIndex))
     return root;
 }
+
+var buildTree2 = function(inorder, postorder) {
+    if(inorder.length === 0 || postorder.length === 0) return null
+    return traversal(inorder, postorder)
+};
+
+const traversal = (inorder, postorder) => {
+    if(postorder.length === 0) return null
+    let rootVal = postorder[postorder.length - 1]
+    let root = new TreeNode(rootVal)
+    if(postorder.length === 1) return root
+
+    let delimiterIndex = 0
+    for(delimiterIndex;delimiterIndex < inorder.length;delimiterIndex++){
+        if(inorder[delimiterIndex] === rootVal) break
+    }
+
+    const leftInorder = inorder.slice(0,delimiterIndex)
+    const leftPostorder = postorder.slice(0,delimiterIndex)
+
+    postorder.pop()
+
+    const rightInorder = inorder.slice(delimiterIndex+1)
+    const rightPostorder = postorder.slice(delimiterIndex)
+
+
+    root.left = traversal(leftInorder,leftPostorder)
+    root.right = traversal(rightInorder,rightPostorder)
+
+    return root
+}
