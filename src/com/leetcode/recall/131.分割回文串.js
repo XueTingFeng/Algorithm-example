4e47692e8424ca724550fc1ac09358cd432624be
unknown <coderxue@qq.com>
2023-03-13 17:37:56 +0800

    131.分割回文串

---
+++ src/com/leetcode/recall/131.分割回文串.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {string[][]}
+ */
+var partition = function(s) {
+    const res = []
+    const path = []
+    const len = s.length
+
+    backtracking(0)
+    return res
+
+    function backtracking(startIndex) {
+    if(startIndex >= len){
+        res.push(Array.from(path))
+        return
+    }
+    for(let i=startIndex;i<len;i++){
+        if(!isPalindrome(s,startIndex,i)) continue
+        path.push(s.slice(startIndex, i + 1))
+        backtracking(i + 1)
+        path.pop()
+    }
+}
+};
+
+const isPalindrome = (s,l,r) => {
+    for(let i=l,j=r;i<j;i++,j--){
+        if(s[i] !== s[j]) return false
+    }
+    return true
+}
